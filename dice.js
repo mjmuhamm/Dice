0103873e6702392102d8ebc24faf001afe4dfe98
mjmuhamm <53790225+mjmuhamm@users.noreply.github.com>
2019-08-28 12:50:13 -0700

    Add files via upload

---
+++ dice.js
@@ -0,0 +1,22 @@
+
+
+    var randomN1 = Math.round(Math.random()*5 +1);
+    var randomDice1 = "images/dice" + randomN1 + ".png";
+
+    var randomN2 = Math.round(Math.random()*5+1);
+    var randomDice2 = "images/dice" + randomN2 + ".png";
+
+      document.querySelectorAll("img")[0].setAttribute("src", randomDice1);
+      document.querySelectorAll("img")[1].setAttribute("src", randomDice2);
+
+if (randomN1 > randomN2) {
+  document.querySelector("h1").innerHTML = "Player 1 Wins!";
+}
+
+else if (randomN1 < randomN2) {
+  document.querySelector("h1").innerHTML = "Player 2 Wins!";
+}
+
+else {
+  document.querySelector("h1").innerHTML = "Draw!";
+}
